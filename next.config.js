5e7736cc02cc10d878bb6a549fff6c31857126a7
MYAIR <ymy0613@gmail.com>
2024-11-01 15:38:28 +0900

    수정: next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -4,13 +4,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
-
-// module.exports = nextConfig;
-
-module.exports = {
   webpack: (config) => {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
   },
 };
+
+module.exports = nextConfig;
